c8d4ab3b446327433a85a97659fb183d64140036
heheboobes <heheboobes@users.noreply.github.com>
2022-01-14 11:00:00 +1000

    Add GameLoop.h fixed timestep design

---
+++ engine/core/GameLoop.cpp
@@ -0,0 +1,91 @@
+#include "GameLoop.h"
+#include "Time.h"
+#include <algorithm>
+#include <chrono>
+#include <thread>
+
+namespace engine {
+namespace core {
+
+GameLoop::GameLoop()
+{
+}
+
+void GameLoop::Run()
+{
+    m_running = true;
+    Time& time = Time::Instance();
+    time.Reset();
+
+    while (m_running)
+    {
+        Tick();
+    }
+}
+
+void GameLoop::Stop()
+{
+    m_running = false;
+}
+
+void GameLoop::Tick()
+{
+    Time& time = Time::Instance();
+    time.Update();
+
+    m_frameTime = time.GetUnscaledDeltaTime();
+    m_frameTime = std::min(m_frameTime, 0.1f);
+
+    m_accumulator += m_frameTime;
+
+    auto fixedStart = std::chrono::high_resolution_clock::now();
+    uint32_t subSteps = 0;
+
+    while (m_accumulator >= m_fixedTimestep && subSteps < m_maxSubSteps)
+    {
+        if (m_fixedUpdateFunc)
+            m_fixedUpdateFunc(m_fixedTimestep);
+
+        m_accumulator -= m_fixedTimestep;
+        subSteps++;
+        m_stats.totalFixedUpdates++;
+    }
+
+    auto fixedEnd = std::chrono::high_resolution_clock::now();
+    m_stats.fixedUpdateTimeMs = std::chrono::duration<float, std::milli>(fixedEnd - fixedStart).count();
+
+    float alpha = m_accumulator / m_fixedTimestep;
+
+    auto updateStart = std::chrono::high_resolution_clock::now();
+    if (m_updateFunc)
+        m_updateFunc(time.GetDeltaTime());
+    auto updateEnd = std::chrono::high_resolution_clock::now();
+    m_stats.updateTimeMs = std::chrono::duration<float, std::milli>(updateEnd - updateStart).count();
+
+    auto renderStart = std::chrono::high_resolution_clock::now();
+    if (m_renderFunc)
+        m_renderFunc(alpha);
+    auto renderEnd = std::chrono::high_resolution_clock::now();
+    m_stats.renderTimeMs = std::chrono::duration<float, std::milli>(renderEnd - renderStart).count();
+
+    m_stats.fps = time.GetFPS();
+    m_stats.frameTimeMs = m_frameTime * 1000.0f;
+    m_stats.interpolationFactor = alpha;
+    m_stats.totalFrames++;
+
+    if (m_targetFPS > 0)
+    {
+        float targetFrameTime = 1.0f / static_cast<float>(m_targetFPS);
+        float elapsed = std::chrono::duration<float>(
+            std::chrono::high_resolution_clock::now() - renderEnd).count();
+        float sleepTime = (targetFrameTime - elapsed) * 1000.0f;
+        if (sleepTime > 1.0f)
+        {
+            std::this_thread::sleep_for(
+                std::chrono::milliseconds(static_cast<int>(sleepTime)));
+        }
+    }
+}
+
+} // namespace core
+} // namespace engine
---
+++ engine/core/GameLoop.h
@@ -0,0 +1,65 @@
+#ifndef ENGINE_CORE_GAME_LOOP_H
+#define ENGINE_CORE_GAME_LOOP_H
+
+#include <functional>
+#include <cstdint>
+
+namespace engine {
+namespace core {
+
+struct GameLoopStats {
+    float fps = 0.0f;
+    float frameTimeMs = 0.0f;
+    float fixedUpdateTimeMs = 0.0f;
+    float updateTimeMs = 0.0f;
+    float renderTimeMs = 0.0f;
+    float interpolationFactor = 0.0f;
+    uint64_t totalFixedUpdates = 0;
+    uint64_t totalFrames = 0;
+};
+
+class GameLoop {
+public:
+    using FixedUpdateFunc = std::function<void(float fixedDt)>;
+    using UpdateFunc = std::function<void(float deltaTime)>;
+    using RenderFunc = std::function<void(float alpha)>;
+
+    GameLoop();
+    ~GameLoop() = default;
+
+    void SetFixedUpdateCallback(FixedUpdateFunc func) { m_fixedUpdateFunc = func; }
+    void SetUpdateCallback(UpdateFunc func) { m_updateFunc = func; }
+    void SetRenderCallback(RenderFunc func) { m_renderFunc = func; }
+
+    void SetTargetFPS(uint32_t fps) { m_targetFPS = fps; }
+    void SetFixedTimestep(float dt) { m_fixedTimestep = dt; }
+    void SetMaxSubSteps(uint32_t steps) { m_maxSubSteps = steps; }
+
+    void Run();
+    void Stop();
+    bool IsRunning() const { return m_running; }
+
+    void Tick();
+
+    const GameLoopStats& GetStats() const { return m_stats; }
+    float GetAlpha() const { return m_accumulator / m_fixedTimestep; }
+
+private:
+    FixedUpdateFunc m_fixedUpdateFunc;
+    UpdateFunc m_updateFunc;
+    RenderFunc m_renderFunc;
+
+    float m_fixedTimestep = 1.0f / 60.0f;
+    float m_accumulator = 0.0f;
+    float m_frameTime = 0.0f;
+    uint32_t m_targetFPS = 144;
+    uint32_t m_maxSubSteps = 5;
+    volatile bool m_running = false;
+
+    GameLoopStats m_stats;
+};
+
+} // namespace core
+} // namespace engine
+
+#endif // ENGINE_CORE_GAME_LOOP_H
