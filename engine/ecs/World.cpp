91df710e86bf9ff538270ec2a2164128a2d9c9be
heheboobes <heheboobes@users.noreply.github.com>
2022-03-15 09:00:00 +1000

    Add World.h ECS orchestrator

---
+++ engine/ecs/World.cpp
@@ -0,0 +1,204 @@
+#include "World.h"
+#include <cassert>
+
+namespace engine {
+namespace ecs {
+
+World::World()
+{
+}
+
+World::~World()
+{
+    Clear();
+}
+
+Entity World::CreateEntity()
+{
+    Entity entity = m_entityManager.CreateEntity();
+    if (entity.IsNull())
+        return Entity::Null();
+
+    ComponentMask emptyMask;
+    Archetype* archetype = GetOrCreateArchetype(emptyMask);
+    if (archetype)
+    {
+        archetype->AddEntity(entity);
+        m_entityArchetypeMap[entity.GetID()] = archetype;
+    }
+
+    for (auto& system : m_systems)
+        system->OnEntityCreated(this, entity);
+
+    return entity;
+}
+
+bool World::DestroyEntity(Entity entity)
+{
+    if (!m_entityManager.IsAlive(entity))
+        return false;
+
+    for (auto& system : m_systems)
+        system->OnEntityDestroyed(this, entity);
+
+    Archetype* arch = GetArchetypeForEntity(entity);
+    if (arch)
+        arch->RemoveEntity(entity);
+
+    m_entityArchetypeMap.erase(entity.GetID());
+    return m_entityManager.DestroyEntity(entity);
+}
+
+bool World::IsAlive(Entity entity) const
+{
+    return m_entityManager.IsAlive(entity);
+}
+
+Archetype* World::GetOrCreateArchetype(const ComponentMask& mask)
+{
+    for (auto& arch : m_archetypes)
+    {
+        if (arch->GetMask() == mask)
+            return arch.get();
+    }
+
+    auto componentTypes = std::vector<ComponentTypeID>();
+    auto componentSizes = std::vector<size_t>();
+    for (ComponentTypeID i = 0; i < MAX_COMPONENT_TYPES; ++i)
+    {
+        if (mask.GetBit(i))
+        {
+            componentTypes.push_back(i);
+            const ComponentTypeInfo* info = ComponentRegistry::Instance().GetTypeInfo(i);
+            componentSizes.push_back(info ? info->size : 0);
+        }
+    }
+
+    auto archetype = std::make_unique<Archetype>(mask, componentTypes, componentSizes);
+    Archetype* ptr = archetype.get();
+    m_archetypes.push_back(std::move(archetype));
+    return ptr;
+}
+
+Archetype* World::GetArchetypeForEntity(Entity entity)
+{
+    auto it = m_entityArchetypeMap.find(entity.GetID());
+    if (it != m_entityArchetypeMap.end())
+        return it->second;
+    return nullptr;
+}
+
+const Archetype* World::GetArchetypeForEntity(Entity entity) const
+{
+    auto it = m_entityArchetypeMap.find(entity.GetID());
+    if (it != m_entityArchetypeMap.end())
+        return it->second;
+    return nullptr;
+}
+
+void World::MoveEntityToCorrectArchetype(Entity entity)
+{
+    const ComponentMask& newMask = m_entityManager.GetComponentMask(entity);
+    Archetype* newArch = GetOrCreateArchetype(newMask);
+    Archetype* oldArch = GetArchetypeForEntity(entity);
+
+    if (oldArch == newArch)
+        return;
+
+    if (oldArch)
+    {
+        Chunk* oldChunk = oldArch->GetChunkForEntity(entity);
+        if (oldChunk)
+        {
+            size_t idx = oldChunk->GetEntityIndex(entity);
+
+            Chunk* newChunk = newArch->FindOrCreateChunk();
+            size_t newIdx = newChunk->GetEntityCount();
+            newChunk->SetEntity(newIdx, entity);
+
+            for (ComponentTypeID typeID : oldChunk->GetComponentTypes())
+            {
+                if (newMask.GetBit(typeID))
+                {
+                    void* srcData = oldChunk->GetComponentData(typeID);
+                    void* dstData = newChunk->GetComponentData(typeID);
+                    if (srcData && dstData)
+                    {
+                        const ComponentTypeInfo* info =
+                            ComponentRegistry::Instance().GetTypeInfo(typeID);
+                        if (info)
+                        {
+                            uint8_t* src = static_cast<uint8_t*>(srcData);
+                            uint8_t* dst = static_cast<uint8_t*>(dstData);
+                            std::memcpy(dst + newIdx * info->size,
+                                       src + idx * info->size,
+                                       info->size);
+                        }
+                    }
+                }
+            }
+
+            newChunk->GetEntityCount();
+            oldChunk->RemoveEntity(idx);
+
+            if (oldArch->IsEmpty())
+            {
+                auto it = std::remove_if(m_archetypes.begin(), m_archetypes.end(),
+                    [oldArch](const std::unique_ptr<Archetype>& a) {
+                        return a.get() == oldArch;
+                    });
+                m_archetypes.erase(it, m_archetypes.end());
+            }
+        }
+    }
+    else
+    {
+        newArch->AddEntity(entity);
+    }
+
+    m_entityArchetypeMap[entity.GetID()] = newArch;
+}
+
+void World::Update(float deltaTime)
+{
+    for (auto& system : m_systems)
+    {
+        if (!system->IsEnabled())
+            continue;
+
+        system->OnUpdate(this, deltaTime);
+    }
+}
+
+void World::Render(float alpha)
+{
+    for (auto& system : m_systems)
+    {
+        if (!system->IsEnabled())
+            continue;
+
+        system->OnRender(this, alpha);
+    }
+}
+
+void World::SortSystems()
+{
+    std::sort(m_systems.begin(), m_systems.end(),
+        [](const std::unique_ptr<ISystem>& a, const std::unique_ptr<ISystem>& b) {
+            return a->GetPriority() < b->GetPriority();
+        });
+}
+
+void World::Clear()
+{
+    for (auto& system : m_systems)
+        system->OnDetach(this);
+
+    m_systems.clear();
+    m_archetypes.clear();
+    m_entityArchetypeMap.clear();
+    m_entityManager.Clear();
+}
+
+} // namespace ecs
+} // namespace engine
---
+++ engine/ecs/World.h
@@ -0,0 +1,162 @@
+#ifndef ENGINE_ECS_WORLD_H
+#define ENGINE_ECS_WORLD_H
+
+#include "Entity.h"
+#include "EntityManager.h"
+#include "Component.h"
+#include "ComponentRegistry.h"
+#include "Archetype.h"
+#include "System.h"
+#include <unordered_map>
+#include <memory>
+#include <vector>
+#include <algorithm>
+
+namespace engine {
+namespace ecs {
+
+class World {
+public:
+    World();
+    ~World();
+
+    World(const World&) = delete;
+    World& operator=(const World&) = delete;
+    World(World&&) = delete;
+    World& operator=(World&&) = delete;
+
+    Entity CreateEntity();
+    bool DestroyEntity(Entity entity);
+    bool IsAlive(Entity entity) const;
+
+    template<typename T, typename... Args>
+    T& AddComponent(Entity entity, Args&&... args)
+    {
+        ComponentTypeID typeID = Component<T>::GetStaticTypeID();
+        ComponentRegistry::Instance().Register<T>();
+
+        if (!m_entityManager.IsAlive(entity))
+        {
+            static T dummy;
+            return dummy;
+        }
+
+        m_entityManager.AddComponentType(entity, typeID);
+        MoveEntityToCorrectArchetype(entity);
+
+        Archetype* arch = GetArchetypeForEntity(entity);
+        if (arch)
+        {
+            Chunk* chunk = arch->GetChunkForEntity(entity);
+            if (chunk)
+            {
+                T* arr = static_cast<T*>(chunk->GetComponentData(typeID));
+                if (arr)
+                {
+                    size_t idx = chunk->GetEntityIndex(entity);
+                    new (&arr[idx]) T(std::forward<Args>(args)...);
+                    return arr[idx];
+                }
+            }
+        }
+
+        static T fallback;
+        return fallback;
+    }
+
+    template<typename T>
+    void RemoveComponent(Entity entity)
+    {
+        ComponentTypeID typeID = Component<T>::GetStaticTypeID();
+        if (!m_entityManager.HasComponent(entity, typeID))
+            return;
+
+        Archetype* oldArch = GetArchetypeForEntity(entity);
+        if (oldArch)
+        {
+            Chunk* chunk = oldArch->GetChunkForEntity(entity);
+            if (chunk)
+            {
+                size_t idx = chunk->GetEntityIndex(entity);
+                T* arr = static_cast<T*>(chunk->GetComponentData(typeID));
+                if (arr)
+                    arr[idx].~T();
+            }
+        }
+
+        m_entityManager.RemoveComponentType(entity, typeID);
+        MoveEntityToCorrectArchetype(entity);
+    }
+
+    template<typename T>
+    T* GetComponent(Entity entity)
+    {
+        ComponentTypeID typeID = Component<T>::GetStaticTypeID();
+        if (!m_entityManager.HasComponent(entity, typeID))
+            return nullptr;
+
+        Archetype* arch = GetArchetypeForEntity(entity);
+        if (!arch) return nullptr;
+
+        Chunk* chunk = arch->GetChunkForEntity(entity);
+        if (!chunk) return nullptr;
+
+        T* arr = static_cast<T*>(chunk->GetComponentData(typeID));
+        if (!arr) return nullptr;
+
+        size_t idx = chunk->GetEntityIndex(entity);
+        return &arr[idx];
+    }
+
+    template<typename T>
+    const T* GetComponent(Entity entity) const
+    {
+        return const_cast<World*>(this)->GetComponent<T>(entity);
+    }
+
+    template<typename T>
+    bool HasComponent(Entity entity) const
+    {
+        return m_entityManager.HasComponent(entity, Component<T>::GetStaticTypeID());
+    }
+
+    template<typename TSystem, typename... Args>
+    TSystem& RegisterSystem(Args&&... args)
+    {
+        auto system = std::make_unique<TSystem>(std::forward<Args>(args)...);
+        TSystem* ptr = system.get();
+        ptr->OnAttach(this);
+        m_systems.push_back(std::move(system));
+        SortSystems();
+        return *ptr;
+    }
+
+    void Update(float deltaTime);
+    void Render(float alpha);
+
+    EntityManager& GetEntityManager() { return m_entityManager; }
+    const EntityManager& GetEntityManager() const { return m_entityManager; }
+
+    Archetype* GetOrCreateArchetype(const ComponentMask& mask);
+    Archetype* GetArchetypeForEntity(Entity entity);
+    const Archetype* GetArchetypeForEntity(Entity entity) const;
+
+    size_t GetArchetypeCount() const { return m_archetypes.size(); }
+    size_t GetSystemCount() const { return m_systems.size(); }
+
+    void Clear();
+
+private:
+    void MoveEntityToCorrectArchetype(Entity entity);
+    void SortSystems();
+
+    EntityManager m_entityManager;
+    std::vector<std::unique_ptr<Archetype>> m_archetypes;
+    std::vector<std::unique_ptr<ISystem>> m_systems;
+    std::unordered_map<uint64_t, Archetype*> m_entityArchetypeMap;
+};
+
+} // namespace ecs
+} // namespace engine
+
+#endif // ENGINE_ECS_WORLD_H
