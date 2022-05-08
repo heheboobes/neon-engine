476097f1297a64ae1424eba32b9c69edddfc4da8
heheboobes <heheboobes@users.noreply.github.com>
2022-05-09 09:00:00 +1000

    Add RenderSystem with sprite iteration

---
+++ engine/systems/RenderSystem.cpp
@@ -0,0 +1,152 @@
+#include "RenderSystem.h"
+#include "../ecs/World.h"
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <cassert>
+
+namespace engine {
+namespace systems {
+
+RenderSystem::RenderSystem()
+    : m_target(nullptr)
+    , m_clearColor(sf::Color::Black)
+    , m_viewportLayer(0)
+    , m_sortByDepth(true)
+    , m_hasCamera(false)
+{
+    SetName("RenderSystem");
+    SetPriority(100);
+    Require<components::SpriteComponent, components::TransformComponent>();
+}
+
+void RenderSystem::SetTarget(sf::RenderTarget* target)
+{
+    m_target = target;
+}
+
+void RenderSystem::SetViewportLayer(int32_t layer)
+{
+    m_viewportLayer = layer;
+}
+
+void RenderSystem::ClearColor(const sf::Color& color)
+{
+    m_clearColor = color;
+}
+
+void RenderSystem::SetSortByDepth(bool enabled)
+{
+    m_sortByDepth = enabled;
+}
+
+void RenderSystem::SetCamera(ecs::Entity cameraEntity)
+{
+    m_cameraEntity = cameraEntity;
+    m_hasCamera = true;
+}
+
+void RenderSystem::OnUpdate(ecs::World* world, float deltaTime)
+{
+    (void)deltaTime;
+    CollectRenderables(world);
+    SortRenderables();
+}
+
+void RenderSystem::OnRender(ecs::World* world, float alpha)
+{
+    (void)world;
+    (void)alpha;
+
+    if (!m_target)
+        return;
+
+    sf::RenderStates states;
+    states.blendMode = sf::BlendAlpha;
+
+    if (m_hasCamera)
+    {
+        auto* camera = world->GetComponent<components::CameraComponent>(m_cameraEntity);
+        if (camera && camera->active)
+        {
+            m_target->setView(camera->view);
+        }
+    }
+
+    SubmitDrawCalls(*m_target);
+    m_renderQueue.clear();
+}
+
+void RenderSystem::CollectRenderables(ecs::World* world)
+{
+    m_renderQueue.clear();
+
+    auto& entityManager = world->GetEntityManager();
+    const auto& archetypes = entityManager.GetArchetypes();
+
+    for (const auto& archetype : archetypes)
+    {
+        if (!archetype->GetMask().IsSupersetOf(GetComponentMask()))
+            continue;
+
+        for (auto& chunk : archetype->GetChunks())
+        {
+            auto* sprites = static_cast<components::SpriteComponent*>(
+                chunk.GetComponentData(components::SpriteComponent::GetStaticTypeID()));
+            auto* transforms = static_cast<components::TransformComponent*>(
+                chunk.GetComponentData(components::TransformComponent::GetStaticTypeID()));
+
+            if (!sprites || !transforms)
+                continue;
+
+            for (size_t i = 0; i < chunk.GetEntityCount(); ++i)
+            {
+                if (!sprites[i].visible || !sprites[i].HasTexture())
+                    continue;
+
+                RenderCommand cmd;
+                cmd.entity = chunk.GetEntity(i);
+                cmd.sprite = &sprites[i];
+                cmd.transform = &transforms[i];
+                cmd.layer = RenderLayer(0, 0, transforms[i].position.z);
+                m_renderQueue.push_back(cmd);
+            }
+        }
+    }
+}
+
+void RenderSystem::SortRenderables()
+{
+    if (!m_sortByDepth)
+        return;
+
+    std::sort(m_renderQueue.begin(), m_renderQueue.end(),
+        [](const RenderCommand& a, const RenderCommand& b) {
+            if (a.layer.layer != b.layer.layer)
+                return a.layer.layer < b.layer.layer;
+            return a.layer.depth < b.layer.depth;
+        });
+}
+
+void RenderSystem::SubmitDrawCalls(sf::RenderTarget& target)
+{
+    target.clear(m_clearColor);
+
+    for (const auto& cmd : m_renderQueue)
+    {
+        sf::Sprite drawSprite = cmd.sprite->sprite;
+        drawSprite.setPosition(cmd.transform->position.x, cmd.transform->position.y);
+        drawSprite.setRotation(cmd.transform->rotation.z);
+        drawSprite.setScale(
+            cmd.transform->scale.x * (cmd.sprite->flipX ? -1.0f : 1.0f),
+            cmd.transform->scale.y * (cmd.sprite->flipY ? -1.0f : 1.0f)
+        );
+
+        sf::Color spriteColor = drawSprite.getColor();
+        spriteColor.a = cmd.sprite->opacity;
+        drawSprite.setColor(spriteColor);
+
+        target.draw(drawSprite);
+    }
+}
+
+} // namespace systems
+} // namespace engine
---
+++ engine/systems/RenderSystem.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include "../ecs/System.h"
+#include "../ecs/World.h"
+#include "../components/SpriteComponent.h"
+#include "../components/TransformComponent.h"
+#include <SFML/Graphics/RenderTarget.hpp>
+#include <SFML/Graphics/RenderStates.hpp>
+#include <vector>
+#include <algorithm>
+
+namespace engine {
+namespace systems {
+
+struct RenderLayer {
+    int32_t layer;
+    int32_t order;
+    float depth;
+
+    RenderLayer() : layer(0), order(0), depth(0.0f) {}
+    RenderLayer(int32_t l, int32_t o, float d) : layer(l), order(o), depth(d) {}
+};
+
+struct RenderCommand {
+    ecs::Entity entity;
+    const components::SpriteComponent* sprite;
+    const components::TransformComponent* transform;
+    RenderLayer layer;
+};
+
+class RenderSystem : public ecs::ISystem {
+public:
+    RenderSystem();
+    void OnUpdate(ecs::World* world, float deltaTime) override;
+    void OnRender(ecs::World* world, float alpha) override;
+
+    void SetTarget(sf::RenderTarget* target);
+    void SetViewportLayer(int32_t layer);
+    void ClearColor(const sf::Color& color);
+    void SetSortByDepth(bool enabled);
+    void SetCamera(ecs::Entity cameraEntity);
+
+private:
+    void CollectRenderables(ecs::World* world);
+    void SortRenderables();
+    void SubmitDrawCalls(sf::RenderTarget& target);
+
+    std::vector<RenderCommand> m_renderQueue;
+    sf::RenderTarget* m_target;
+    sf::Color m_clearColor;
+    int32_t m_viewportLayer;
+    bool m_sortByDepth;
+    ecs::Entity m_cameraEntity;
+    bool m_hasCamera;
+};
+
+} // namespace systems
+} // namespace engine
